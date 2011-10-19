9e3370852f1ab34bfccbcca38f594c9671d8000c
goncalopp <goncalopp@gmail.com>
2011-10-19 19:30:21 +0100

    changes to cpu.py: support for zero-duration tasks, step_callbacks, bugfixes

--- sim/cpu.py
+++ sim/cpu.py
@@ -1,10 +1,12 @@
 import logging
 
-class ConclusionException( Exception ):
+class TaskConclusionException( Exception ):
+    pass
+class TaskZeroDuration( Exception ):
     pass
 
 class CpuTask:
-    def __init__(self, task_name, task_duration, on_step= lambda:None, on_conclusion= lambda:None):
+    def __init__(self, task_name, task_duration, on_conclusion= lambda:None, on_step= lambda:None):
         self.task_name, self.task_duration, self.on_step, self.on_conclusion= task_name, task_duration, on_step, on_conclusion
         self.done=0
         self.concluded= False
@@ -12,32 +14,42 @@ class CpuTask:
     def step(self):
         if self.concluded:
             raise Exception("Stepping concluded task")
+        if self.task_duration==0:
+            self.on_conclusion()
+            raise TaskZeroDuration()
         self.done+= 1
         self.on_step()
         if self.done == self.task_duration:
             self.on_conclusion()
             self.concluded= True
-            raise ConclusionException();
+            raise TaskConclusionException();
 
 class Cpu:
-    def __init__(self, step_functions=[]):
+    def __init__(self):
         self.rtc=0
         self.tasks= []
-        self.step_functions= step_functions
+        self.step_functions= []
 
     def step( self ):
+        stepped= False
+        while not stepped:
+                try:
+                    self.tasks[0].step()
+                    logging.info("CPU stepped: "+self.tasks[0].task_name)
+                    stepped= True
+                except IndexError:
+                    #no tasks
+                    logging.warning("CPU stepped while executing no task")
+                    stepped=True
+                except TaskConclusionException:
+                    logging.info("CPU stepped and concluded: "+self.tasks[0].task_name)
+                    self.tasks.pop(0)
+                    stepped= True
+                except TaskZeroDuration:
+                    logging.info("CPU completed task in 0 clocks: "+self.tasks[0].task_name)
+                    self.tasks.pop(0)
         self.rtc+=1
-        if len(tasks)==0:
-            logging.warning("CPU stepped while executing no task")
-        else:
-            try:
-                self.tasks[0].step()
-                logging.info("CPU stepped executing "+self.tasks[0].task_name)
-            except ConclusionException:
-                logging.info("CPU task concluded: "+self.tasks[0].task_name)
-                self.tasks.pop(0)
-                
-        for f in self.step_functions(): #step all functions we were required to, on instantiation
+        for f in self.step_functions: #step all functions we were required to, on instantiation
             f()
             
     def add_task( self, task ):
@@ -49,5 +61,9 @@ class Cpu:
         task.task_name+=" [INTERRUPT]"
         self.tasks.insert(0, task)          # a interrupt is immediatly executed
         
+    def add_step_callback(self, f):
+        self.step_functions.append( f )
     
+    def remove_step_callback(self, f):
+        self.step_functions.remove( f )
     
