3d07998d2fc1acba1c41e2e64e3214bf6c88342c
goncalopp <goncalopp@gmail.com>
2011-10-21 12:50:52 +0100

    cpu_instruction.py: add functions to convert string<-->Instruction

--- os/hardware/cpu_instruction.py
+++ os/hardware/cpu_instruction.py
@@ -7,10 +7,26 @@ opcodes= \
     "INT": 1,   #generate software interrupt [a1]
     "OFF": 2,   #"turn system off" (python exception) 
     }
-    
-for k,v in opcodes.items():
-    locals()[k]=v
 
 class Instruction:
     def __init__(self, op, a1):
         self.op, self.a1= op, a1
+    def __repr__(self):
+        try:
+            return "\t".join( (opcodes_reverse[self.op], str(self.a1)) )
+        except KeyError:
+            raise UnknownOpcode()
+
+def instructionFromString(s):
+    try:
+        op, a1= s.split("\t")
+        a1= int(a1)
+        op= opcodes[op]
+        return Instruction(op, a1)
+    except KeyError:
+            raise UnknownOpcode()
+
+
+opcodes_reverse= dict((v,k) for k, v in opcodes.items())    #construction dictionary from opcode to opname
+for k,v in opcodes.items():                   #add opcodes as local variables
+    locals()[k]=v
