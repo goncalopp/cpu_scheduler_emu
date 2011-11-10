90ea6ae16fd2ac2e4b1b13a6609be5b3124e940b
goncalopp <goncalopp@gmail.com>
2011-11-10 01:01:01 +0000

    refactor boot. Main program is now run.py

--- boot.py
+++ boot.py
@@ -1,74 +1,59 @@
 import sys,os
-sys.path.append("operating_system")
 from kernel import Kernel
 from dispatcher import NoMoreProcesses
 from hardware.machine import Machine
-import config
-import program_generation
 import process_statistics
 
-def cli_choice(question, answers):
-    n= len(answers)
-    print question
-    for i,answer in enumerate(answers):
-        print str(i+1)+")\t"+answer
-    while True:
-        print ">",
-        try:
-            return answers[int(raw_input())-1]
-        except (ValueError, IndexError):
-            print "Invalid choice"
-
+MAX_CYCLES= 10**8 #run for a maximum of 10^8 cycles, for sanity's sake
 TRACE_FILE= "process_trace.txt"
 STAT_FILE= "statistics.txt"
-CONFIG_DIR= "configs"
 
-config_files= os.listdir("configs")
-#cfg_file= cli_choice( "Please choose the config file to use:",config_files )
-cfg_file= "ten_processes.cfg" #uncomment here and comment previous line for no prompts 
+def simulate(programs, io_operation_duration=100, sim_runtime=None):
+    print "creating virtual hardware"
+    my_pc= Machine()
+    my_pc.io.set_io_operation_time( io_operation_duration )
+    print '"bootstraping" OS'
+    my_os= Kernel( my_pc )
+    tracer= process_statistics.ProcessTracer( my_os )   #traces processes
 
-print "parsing config file"
-cfg= config.configFromFile( os.path.join( CONFIG_DIR, cfg_file ))
-print "generating programs"
-programs= program_generation.generateProgramsFromConfig(cfg)
+    print "creating processes"
+    for program in programs:
+        pcb= my_os.process_manager.start_program( program )
+        program.pid= pcb.pid    #hack to save the pid of each program (for calculating statistics)
+        program.process_start_address= pcb.start_address    #hack for correctness tests
+        if not hasattr(program, "duration"):
+            program.duration=0
+        program.duration+=1     #since the programs are added one instruction by the OS (to signal termination)
+    program_durations= dict([ (program.pid, program.duration) for program in programs])
 
-print "creating virtual hardware"
-my_pc= Machine()
-my_pc.io.set_io_operation_time( cfg.iotime )
-print '"bootstraping" OS'
-my_os= Kernel( my_pc )
-tracer= process_statistics.ProcessTracer( my_os )   #traces processes
+    print "starting simulation"
+    my_os.kickstart()
 
-print "creating processes"
-for program in programs:
-    pcb= my_os.process_manager.start_program( program )
-    program.pid= pcb.pid    #hack to save the pid of each program (for calculating statistics)
-    program.duration+=1     #since the programs are added one instruction by the OS (to signal termination)
-program_durations= dict([ (program.pid, program.duration) for program in programs])
+    for cycle in xrange( MAX_CYCLES): 
+        if cycle%1000==0:
+            print "executing simulation: cycle ", cycle
+        if cycle== sim_runtime:
+            print "reached simulation runtime. Simulation stopped."
+            break
+        try:
+            my_pc.step()
+        except NoMoreProcesses:
+            print "At cycle",cycle, "all processes have finished. Simulation stopped"
+            break
 
-print "starting simulation"
-my_os.kickstart()
+    my_os.shutdown()
 
-for cycle in xrange(10**8): #run for a maximum of 10^8 cycles, for sanity's sake
-    if cycle%1000==0:
-        print "executing simulation: cycle ", cycle
-    if cycle== cfg.runtime:
-        print "reached simulation runtime. Simulation stopped."
-        break
-    try:
-        my_pc.step()
-    except NoMoreProcesses:
-        print "At cycle",cycle, "all processes have finished. Simulation stopped"
-        break
+    tracer.process(program_durations)
+    print "writing process trace to "+TRACE_FILE+" (PID 0 is the idle process)"
+    trace= str(tracer)
+    #print trace
+    open(TRACE_FILE, "w").write( trace )
+    print "writing statistics to "+STAT_FILE
+    stats= tracer.get_statistics(io_operation_duration, program_durations)
+    #print stats[:200]   #only first 200 chars
+    open(STAT_FILE, "w").write( stats )
+    print "Additional execution information can be found in the debug logs (os.log.tsv, hardware.log.tsv)"
+    return my_pc, my_os #needed for correctness testing, since it checks final ram contents
 
-my_os.shutdown()
 
-tracer.process(program_durations)
-print "writing process trace to "+TRACE_FILE+" (PID 0 is the idle process)"
-trace= str(tracer)
-#print trace
-open(TRACE_FILE, "w").write( trace )
-print "writing statistics to "+STAT_FILE
-stats= tracer.get_statistics(cfg.iotime, program_durations)
-print stats[:200]   #only first 200 chars
-open(STAT_FILE, "w").write( stats )
+
---
+++ run.py
@@ -0,0 +1,46 @@
+CONFIG_DIR= "configs"
+
+import sys, os
+sys.path.append("operating_system")
+import boot, config, program_generation
+
+
+def cli_choice(question, answers, return_index=False):
+    n= len(answers)
+    print question
+    for i,answer in enumerate(answers):
+        print str(i+1)+")\t"+answer
+    while True:
+        print ">",
+        try:
+            index= int(raw_input())-1
+            if return_index:
+                return index
+            else: 
+                return answers[index]
+        except (ValueError, IndexError):
+            print "Invalid choice"
+
+def run_sim_from_config( cfg_file ):
+    print "parsing config file"
+    cfg= config.configFromFile( os.path.join( CONFIG_DIR, cfg_file ))
+    print "generating and assembling programs (from config file)"
+    programs= program_generation.generateProgramsFromConfig(cfg)
+    boot.simulate(programs, cfg.iotime)
+
+def run_correctness_tests():
+    print "running correctness tests"
+    from hardware import verification_programs
+    tests= verification_programs.tests
+    programs= [t.program for t in tests]
+    pc, os= boot.simulate( programs )
+    for test in tests:
+        test.verify( pc.ram, test.program.process_start_address )
+        print "test",test.name,"passed!"    #since if it doesn't, a exception is raised
+
+if cli_choice("What to do?", ("Run simulation from a config file","Run correctness checks"), return_index=True)==0:
+    config_files= os.listdir( CONFIG_DIR)
+    cfg_file= cli_choice( "Please choose the config file to use:",config_files )
+    run_sim_from_config( cfg_file )
+else:
+    run_correctness_tests()
